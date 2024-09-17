19b22f64e5966422771ebe7111c7a8c5b1f503dc
samirpoudel <poudelsamir2016@gmail.com>
2024-09-17 17:06:54 +0930

    simple carpet cost calculator project

---
+++ Carpet.java
@@ -0,0 +1,13 @@
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost){
+        this.cost = cost;
+        if (cost<0){
+            cost = 0;
+        }
+    }
+    public double getCost(){
+        return cost;
+    }
+}
